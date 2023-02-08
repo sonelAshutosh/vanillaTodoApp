2d589f8774ab824d0e70b5b06507dd0349f1d8e1
sonelAshutosh <ashutoshhoonmain@gmail.com>
2023-02-08 23:06:19 +0530

    Added the functionality for permanent storage of todos for a single user

--- script.js
+++ script.js
@@ -9,22 +9,37 @@ addTodo.addEventListener('click', () => {
     alert('Enter a value !');
   } else {
     todoArray.push(todo.value);
-    todo.value = '';
     todosContainer.innerHTML = '';
-    todoArray.forEach((todo) => {
-      let newTodo = document.createElement('li');
-      newTodo.innerHTML = `<input type="button" value="-" onclick="deleteTodo(${todoArray.indexOf(
-        todo
-      )})" />${todo}`;
-      newTodo.setAttribute('id', todoArray.indexOf(todo));
-      todosContainer.appendChild(newTodo);
-    });
+    setLocalStorage();
+    populateList();
+
+    todo.value = '';
   }
 });
 
 function deleteTodo(id) {
   todoArray = todoArray.filter((todo) => todoArray.indexOf(todo) !== id);
   todosContainer.innerHTML = '';
+  populateList();
+  setLocalStorage();
+}
+
+function setLocalStorage() {
+  localStorage.setItem('todos', JSON.stringify(todoArray));
+}
+
+function getLocalStorage() {
+  let temp = localStorage.getItem('todos');
+  if (temp !== null) {
+    console.log(temp);
+    todoArray = JSON.parse(temp);
+  } else {
+    todoArray = [];
+  }
+  populateList();
+}
+
+function populateList() {
   todoArray.forEach((todo) => {
     let newTodo = document.createElement('li');
     newTodo.innerHTML = `<input type="button" value="-" onclick="deleteTodo(${todoArray.indexOf(
