3bb27664a06538899913ac693c3af9f265b065c9
abhijeet <abhijeetsinghbhainser@gmail.com>
2023-02-11 22:45:24 +0530

    partial timer functionaliy added

--- script.js
+++ script.js
@@ -4,17 +4,22 @@ let navbarUl = document.getElementById('navbar-ul');
 let navOpenButton = document.getElementById('navOpenButton');
 let todosContainer = document.getElementById('todosContainer');
 let navCloseButton = document.getElementById('navCloseButton');
+let min = document.getElementById('number_minutes')
+let sec = document.getElementById('number_seconds')
+// let hr = document.getElementById('number_hours')
 let todoArray = [];
 
 addTodo.addEventListener('click', () => {
   if (todo.value == '') {
     alert('Enter a value !');
   } else {
-    todoArray.push(todo.value);
+    console.log(todoArray)
+    todoArray.push([todo.value, min.value, sec.value]);
     todosContainer.innerHTML = '';
+    min.value = 0
+    sec.value = 0
     setLocalStorage();
     populateList();
-
     todo.value = '';
   }
 });
@@ -46,9 +51,18 @@ function populateList() {
     let newTodo = document.createElement('li');
     newTodo.innerHTML = `<input type="button" value="-" onclick="deleteTodo(${todoArray.indexOf(
       todo
-    )})" />${todo}`;
+    )})" />${todo[0]} <span id="time${todoArray.indexOf(todo)}"></span>`;
     newTodo.setAttribute('id', todoArray.indexOf(todo));
     todosContainer.appendChild(newTodo);
+  });
+  triggerTimer()
+}
+
+function triggerTimer(){
+  todoArray.forEach((todo) => {
+    var minutes = parseInt(todo[1]*60) + parseInt(todo[2])
+    display = document.querySelector(`#time${todoArray.indexOf(todo)}`);
+    startTimer(minutes, display);
   });
 }
 
@@ -58,3 +72,29 @@ navCloseButton.addEventListener('click', () => {
 navOpenButton.addEventListener('click', () => {
   navbarUl.style.top = '-240px';
 });
+
+// adding a timer function
+
+function startTimer(duration, display) {
+  var timer = duration, minutes, seconds;
+  setInterval(function () {
+    minutes = parseInt(timer / 60, 10);
+    seconds = parseInt(timer % 60, 10);
+    minutes = minutes < 10 ? "0" + minutes : minutes;
+    seconds = seconds < 10 ? "0" + seconds : seconds;
+
+    display.textContent = minutes + ":" + seconds;
+
+    if (--timer < 0) {
+      timer = duration;
+    }
+  }, 1000);
+}
+
+// to constraint the timer with numerical values
+function isNumberKey(evt) {
+  var charCode = (evt.which) ? evt.which : event.keyCode;
+  if (charCode > 31 && (charCode < 48 || charCode > 57))
+    return false;
+  return true;
+}
